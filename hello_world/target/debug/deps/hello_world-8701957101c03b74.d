54eb88eb343901d436dc044c3756ff7966f19bd4
k-furusho <kfurusho0509@gmail.com>
2024-10-26 12:07:08 +0900

    use cargo

---
+++ hello_world/target/debug/deps/hello_world-8701957101c03b74.d
@@ -0,0 +1,5 @@
+/Users/furushokazuya/bbtech/furusho/hello-rust/hello_world/target/debug/deps/hello_world-8701957101c03b74: src/main.rs
+
+/Users/furushokazuya/bbtech/furusho/hello-rust/hello_world/target/debug/deps/hello_world-8701957101c03b74.d: src/main.rs
+
+src/main.rs:

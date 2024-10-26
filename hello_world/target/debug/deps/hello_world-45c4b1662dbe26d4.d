659297261908e74e980cf284fa4cb31d355daa7a
k-furusho <kfurusho0509@gmail.com>
2024-10-26 12:21:58 +0900

    setting rustfmt, clippy

---
+++ hello_world/target/debug/deps/hello_world-45c4b1662dbe26d4.d
@@ -0,0 +1,5 @@
+/Users/furushokazuya/bbtech/furusho/hello-rust/hello_world/target/debug/deps/libhello_world-45c4b1662dbe26d4.rmeta: src/main.rs
+
+/Users/furushokazuya/bbtech/furusho/hello-rust/hello_world/target/debug/deps/hello_world-45c4b1662dbe26d4.d: src/main.rs
+
+src/main.rs:

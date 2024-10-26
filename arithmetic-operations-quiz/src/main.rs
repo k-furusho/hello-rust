ec1ad51858ea6ace58d858471909e6eeae26fe60
k-furusho <kfurusho0509@gmail.com>
2024-10-26 23:15:12 +0900

    add:減算(i32型)

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -7,12 +7,27 @@ fn main() {
     // 標準入力から1行取得し、ans_input に代入する
     std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    // ans_inputから trim()で改行を取り除き parse()で整数(u32)型に変換する
-    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+    // ans_inputから trim()で改行を取り除き parse()で整数(i32)型に変換する
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
     dbg!(ans_input); // => cargo runした後にキーボードで入力した値が確認できる
     if dbg!(ans_input == 1 + 1) {
         println!("正解！");
     } else {
         println!("不正解！")
     }
+
+    println!("1 - 4 = ??");
+    println!("?? の値を入力してください:");
+
+    let mut ans_input = String::new(); // ユーザーからの回答を保持する変数
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+    let ans_input = ans_input.trim().parse::<i32>().unwrap();
+    dbg!(ans_input);
+    if dbg!(ans_input == 1 - 4) {
+        println!("正解!");
+    } else {
+        println!("不正解!");
+    }
+    println!("i32 が扱えるデータ範囲: {} ~ {}", i32::MIN, i32::MAX);
+    println!("u32 が扱えるデータ範囲: {} ~ {}", u32::MIN, u32::MAX);
 }

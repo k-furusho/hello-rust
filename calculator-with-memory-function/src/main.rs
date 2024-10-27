94a281fca0f88851f7026937f5483733822a7909
k-furusho <kfurusho0509@gmail.com>
2024-10-27 18:38:37 +0900

    add:memory

--- calculator-with-memory-function/src/main.rs
+++ calculator-with-memory-function/src/main.rs
@@ -1,6 +1,9 @@
 use std::io::stdin;
 
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
+
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
         let line = line.unwrap();
@@ -10,12 +13,33 @@ fn main() {
         // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
+        if tokens[0] == "mem+" {
+            memory += prev_result;
+            print_value(memory);
+            continue;
+        } else if tokens[0] == "mem-" {
+            memory -= prev_result;
+            print_value(memory);
+            continue;
+        }
+
         // 式の計算
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[2].parse().unwrap();
+        let left = if tokens[0] == "mem" {
+            memory
+        } else {
+            tokens[0].parse().unwrap()
+        };
+        let right = if tokens[2] == "mem" {
+            memory
+        } else {
+            tokens[2].parse().unwrap()
+        };
+
         let result = calculate(left, right, tokens[1]);
         // 結果の表示
         print_value(result);
+
+        prev_result = result;
     }
 }
 
